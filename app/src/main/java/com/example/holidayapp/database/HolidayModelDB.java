5c360377a7a2cc63cb10db89bfdcc5050e5f176b
fajarhr <fazarramadhan9@gmail.com>
2020-05-09 16:36:09 +0700

    Fitur API

--- app/src/main/java/com/example/holidayapp/database/HolidayModelDB.java
+++ app/src/main/java/com/example/holidayapp/database/HolidayModelDB.java
@@ -10,11 +10,14 @@ public class HolidayModelDB {
     private String date;
 
     @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int id;
+
     @ColumnInfo(name = "name")
-    private double name;
+    private String name;
 
     @ColumnInfo(name = "country")
-    private int country;
+    private String country;
 
     public String getDate() {
         return date;
@@ -24,19 +27,27 @@ public class HolidayModelDB {
         this.date = date;
     }
 
-    public double getName() {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
         return name;
     }
 
-    public void setName(double name) {
+    public void setName(String name) {
         this.name = name;
     }
 
-    public int getCountry() {
+    public String getCountry() {
         return country;
     }
 
-    public void setCountry(int country) {
+    public void setCountry(String country) {
         this.country = country;
     }
 }
